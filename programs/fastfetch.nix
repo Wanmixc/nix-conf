6b8ba92765cea9bd674fda7d9f338de60c93283a
wanmixc <wanmixc@gmail.com>
2026-09-16 09:38:25 +0700

    🧰update(conf fastfetch)

--- programs/fastfetch.nix
+++ programs/fastfetch.nix
@@ -3,4 +3,5 @@
   home.packages = [ pkgs.fastfetch ];
 
   xdg.configFile."fastfetch/config.jsonc".source = ./fastfetch/config.jsonc;
+  xdg.configFile."fastfetch/logo.txt".source = ./fastfetch/logo.txt;
 }
